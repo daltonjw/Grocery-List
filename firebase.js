c174039b9b648c5bdad876f790bcc41e57485ce8
daltonjw <dalton.jw@gmail.com>
2017-03-16 08:53:05 -0500

    Adding firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.7.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC7coLIY6pX2ntPwZPv58WpKXgBlo4BATY",
+    authDomain: "grocery-list-app-3f7ee.firebaseapp.com",
+    databaseURL: "https://grocery-list-app-3f7ee.firebaseio.com",
+    storageBucket: "grocery-list-app-3f7ee.appspot.com",
+    messagingSenderId: "255300093561"
+  };
+  firebase.initializeApp(config);
+</script>

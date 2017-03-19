6296a01ebad3b230312efdd96f3cc60040800312
daltonjw <dalton.jw@gmail.com>
2017-03-18 23:03:15 -0500

    Updating to add 'adding' funcitonality to firebase and listing all items to the div

--- app2.js
+++ app2.js
@@ -12,21 +12,52 @@ firebase.initializeApp(config);
 //Create variable to reference database
 var database = firebase.database();
 
+//Adding items to the list
 //On click of add item button
 $("#add-item-button").on("click", function(event) {
   // Prevent Form from Refreshing
   event.preventDefault();
-  // Get the input values
+  // Get the input values from the html
   var localGroceryItem = $("#gocery-item-input").val().trim();
   //log values for testing purposes
   console.log(localGroceryItem);
-  database.ref().push({
-    name: localGroceryItem
-  });
-
-  //
-  var localGroceryItem = $("#gocery-item-input").val().trim();
-    // Prevent Form from Refreshing (return false)
-    // return false;
+  //Push localGroceryItem as value to firebase with timestamp as key
+  database.ref().push().set(localGroceryItem);
+  //Empty the html
+  $("#gocery-item-input").val("");
 });
+
+
+//Displaying items in the html
+//Every time the database changes, requery and rewrite all list items
+database.ref().on("value", function(snapshot) {
+  console.log(snapshot.val());
+  var obj = snapshot.val();
+  console.log(obj);
+  //Loop over each item in the snapshot opbject returned by firebase
+  for(var prop in obj) {
+    //not sure if the following line will be useful at some point
+    // if(obj.hasOwnProperty(prop))
+    //log each value to the console for testing purposes
+    console.log(obj[prop]);    
+    var itemName = obj[prop];
+    //building hmls to list object and appending "X" button
+    var listItem = $("<p>");
+    listItem.append(" " + itemName);
+    // Create a button with unique identifiers based on what number it is in the list. Again use jQuery to do this.
+    // Give our button a data attribute called data-item-count and a class called "checkbox".
+    // Lastly append a letter X inside.
+    var closeItem = $("<button>");
+    // closeItem.attr("data-item-count", itemCount);
+    closeItem.addClass("checkbox");
+    closeItem.append("X");
+    listItem = listItem.prepend(closeItem);
+    //Prepend each p tag and button to the div
+    $("#grocery-list-div").prepend(listItem);
+  }
+});
+
+
+//Deleting items from the database
+
 

55eee7aea943432320cf08d9f350033b1e4fd829
daltonjw <dalton.jw@gmail.com>
2017-03-19 21:47:43 -0500

    Updating app2.js

--- app2.js
+++ app2.js
@@ -12,6 +12,8 @@ firebase.initializeApp(config);
 //Create variable to reference database
 var database = firebase.database();
 
+// var rootRef = new Firebase('https://grocery-list-app-3f7ee.firebaseio.com/web/data');
+
 //Adding items to the list
 //On click of add item button
 $("#add-item-button").on("click", function(event) {
@@ -31,6 +33,7 @@ $("#add-item-button").on("click", function(event) {
 //Displaying items in the html
 //Every time the database changes, requery and rewrite all list items
 database.ref().on("value", function(snapshot) {
+  $("#grocery-list-div").empty();
   console.log(snapshot.val());
   var obj = snapshot.val();
   console.log(obj);
@@ -39,10 +42,12 @@ database.ref().on("value", function(snapshot) {
     //not sure if the following line will be useful at some point
     // if(obj.hasOwnProperty(prop))
     //log each value to the console for testing purposes
-    console.log(obj[prop]);    
+    console.log(obj[prop]);
+    console.log(prop);    
     var itemName = obj[prop];
     //building hmls to list object and appending "X" button
     var listItem = $("<p>");
+    listItem.attr("id", prop);
     listItem.append(" " + itemName);
     // Create a button with unique identifiers based on what number it is in the list. Again use jQuery to do this.
     // Give our button a data attribute called data-item-count and a class called "checkbox".
@@ -50,6 +55,7 @@ database.ref().on("value", function(snapshot) {
     var closeItem = $("<button>");
     // closeItem.attr("data-item-count", itemCount);
     closeItem.addClass("checkbox");
+    closeItem.attr("id", prop);
     closeItem.append("X");
     listItem = listItem.prepend(closeItem);
     //Prepend each p tag and button to the div
@@ -59,5 +65,11 @@ database.ref().on("value", function(snapshot) {
 
 
 //Deleting items from the database
-
+$(document.body).on("click", ".checkbox", function() {
+  // Get the itemNumber of the button from its data attribute.
+  var itemName = $(this).attr("id");
+  // console.log(this);
+  console.log(database.ref());
+  // console.log(rootRef.child(itemName));
+});
 
